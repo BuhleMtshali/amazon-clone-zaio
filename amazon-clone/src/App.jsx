33103dbba981162703786bc33cce432eedfc30f4
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-28 13:21:45 +0200

    Updated the routers

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Products from './components/Products';
 import './App.css'
 
 function App() {
   return (
+    <>
+    <Header />
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/products' element={<Products />} />
+    </Routes>
+      
+    </>
     
-    <h1>amazon project</h1>
   )
 }
 
