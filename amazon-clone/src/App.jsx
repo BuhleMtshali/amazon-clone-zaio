d948d641db1378fb6dca1bb27e67a0a7726629fa
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-28 15:37:02 +0200

    updated the nav

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Header from './components/Header';
+import Header from './components/layout/Header';
 import Products from './components/Products';
 import './App.css'
 
---
+++ amazon-clone/src/components/layout/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav>
+        <div className="header">
+                <Link to={"/home"}>
+                <img src="/logo.png" alt="logo image" className="logo-img"/>
+                </Link>
+                <div className="header_search">
+                <input type="search" name="search-icon" id="search-icon" />
+                <SearchIcon className="search-btn"/>
+                </div>
+                {/* end of header search */}
+            <div className="header_nav">
+                <Link to={"/home"}>
+                <div className="header_option">
+                <span className="header_optionLineOne">Hello Guest</span>
+                <span className="header_optionLineTwo">Orders</span>
+                </div>
+                </Link>
+                <div className="header_option">
+                <span className="header_optionLineOne">Your</span>
+                <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <Link to={"/home"}>
+                <div className="header_optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header_optionLineTwo header_basketCount">0</span>
+                </div>
+                </Link>
+            </div>
+            {/* end of header_nav */}
+
+        </div>
+        </nav>
+    )
+}
+
+export default Header;
