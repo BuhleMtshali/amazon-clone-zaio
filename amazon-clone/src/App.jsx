57a75405438b41945893763afa97847168a8fc41
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-28 17:16:33 +0200

    updated the Product details

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/layout/Header';
 import Products from './components/Products';
+import ProductDetails from './components/ProductDetails';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path='/home' element={<Home />} />
       <Route path='/products' element={<Products />} />
+      <Route path='/product-details/:id' element={<ProductDetails />} />
     </Routes>
       
     </>
