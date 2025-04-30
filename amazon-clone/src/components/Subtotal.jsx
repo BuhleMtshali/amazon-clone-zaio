986ed17a07a99a57083c50471ac4785e25f60fb6
Buhle <mtshalizanobuhle7@gmail.com>
2025-04-30 23:12:20 +0200

    updated the subtotal

--- amazon-clone/src/components/Subtotal.jsx
+++ amazon-clone/src/components/Subtotal.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from 'react';
+import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router-dom'
 
 const Subtotal = () => {
-  return (
-    <div>Subtotal</div>
+  const history = useHistory();
+  const [{basket, user}, dispatch] = useStateValue();
+
+  return(
+    <div className="subTotal">
+      <CurrencyFormat renderText={(value) => (
+        <>
+        <p>
+          subtotal ({basket.length} items): {value}
+        </p>
+        </>
+      )}
+      decimalScale={2}
+      value={getBasketTotal(basket)}
+      displayType={'text'}
+      thousandSeparator={true}
+      prefix={'$'}
+      />
+    {user && (
+      <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
+    )}
+    {!user && (
+      <button onClick={e => history.push('/login')}>Sign in or create account to complete checkout</button>
+    )}
+    </div>
   )
 }
 
-export default Subtotal
+export default Subtotal;
